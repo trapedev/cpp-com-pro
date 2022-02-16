096fbfa31a02ca649e3007641c4acb07705747c4
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-02-16 22:30:57 +0900

    A

---
+++ exercises/atcoder/abc/236/a.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define rep(i,n) for(int i = 0; i < (n); i++)
+#define IntToStr(n) to_string(n) //整数を文字列に変換
+#define StrToInt(s) stoi(s) //文字列を整数に変換 
+#define rev(s) reverse(s.begin(), s.end()) //文字列反転
+#define out(n) cout << n << endl
+
+using ll = long long;
+using namespace std;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+const ll mod = 1e9 + 7;
+const int INF = 1000000001;
+
+void solve(){
+}
+
+int main(){
+    string s;
+    int a, b;
+    cin >> s >> a >> b;
+    char A = s[b - 1];
+    char B = s[a - 1];
+    s[a - 1] = A;
+    s[b - 1] = B;
+    cout << s << endl;
+    return 0;
+}
