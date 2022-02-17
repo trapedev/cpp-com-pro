5663f5deb40c47a8f871d7c4e0c27cd29189df49
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-02-17 15:15:42 +0900

    20

---
+++ exercises/leetcode/20.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+#define rep(i,n) for(int i = 0; i < (n); i++)
+#define IntToStr(n) to_string(n) //整数を文字列に変換
+#define StrToInt(s) stoi(s) //文字列を整数に変換 
+#define rev(s) reverse(s.begin(), s.end()) //文字列反転
+#define out(n) cout << n << endl
+
+using ll = long long;
+using namespace std;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+const ll mod = 1e9 + 7;
+const int INF = 1000000001;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> t;
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                t.push(s[i]);
+            } else if(s[i] == ')') {
+                if(i == 0 ||  t.empty() || t.top() != '(') {
+                    return false;
+                }
+                t.pop();
+            } else if(s[i] == ']') {
+                if(i == 0 || t.empty() || t.top() != '[') {
+                    return false;
+                }
+                t.pop();
+            } else if(s[i] == '}') {
+                if(i == 0 || t.empty() || t.top() != '{') {
+                    return false;
+                }
+                t.pop();
+            }
+        }
+        return t.empty();
+    }
+};
+
+void solve(){
+
+}
+
+int main(){
+    
+    return 0;
+}
