41509148fc0d3f007731f35ceb7920426ecb795f
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-02-20 22:25:31 +0900

    C

---
+++ exercises/atcoder/abc/231/c.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+#define rep(i,n) for(int i = 0; i < (n); i++)
+#define IntToStr(n) to_string(n) //整数を文字列に変換
+#define StrToInt(s) stoi(s) //文字列を整数に変換 
+#define rev(s) reverse(s.begin(), s.end()) //文字列反転
+#define out(n) cout << n << endl
+
+/*
+queue: https://cpprefjp.github.io/reference/queue/queue.html
+stack: https://cpprefjp.github.io/reference/stack.html
+permutation: https://cpprefjp.github.io/reference/algorithm/next_permutation.html
+*/
+
+const ll mod = 1e9 + 7;
+const int INF = 1000000001;
+
+void solve(){
+
+}
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    for(int i = 0; i < q; i++){
+        int x;
+        cin >> x;
+        int s = n;
+        int l = -1;
+        while (s - l > 1)
+        {
+            int m = (s + l)/2;
+            if(a[m] >= x){
+                s = m;
+            }else{
+                l = m;
+            }
+        }
+        cout << n - s << endl; 
+    }
+    return 0;
+}
