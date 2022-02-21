46fda60a68883f773fc722e1b23421d2280656aa
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-02-21 21:15:37 +0900

    D

---
+++ exercises/atcoder/abc/240/d.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+#define rep(i,n) for(int i = 0; i < (n); i++)
+#define IntToStr(n) to_string(n) //整数を文字列に変換
+#define StrToInt(s) stoi(s) //文字列を整数に変換 
+#define rev(s) reverse(s.begin(), s.end()) //文字列反転
+#define out(n) cout << n << endl
+
+/*
+queue: https://cpprefjp.github.io/reference/queue/queue.html
+stack: https://cpprefjp.github.io/reference/stack.html
+permutation: https://cpprefjp.github.io/reference/algorithm/next_permutation.html
+*/
+
+const ll mod = 1e9 + 7;
+const int INF = 1000000001;
+
+void solve(){
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<P> tube;
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        cnt++;
+        if(tube.empty() || tube.back().first != a){
+            tube.emplace_back(a, 1);
+        }else{
+            tube.back().second++;
+            if(tube.back().second == a){
+                cnt -= a;
+                tube.pop_back();
+            }
+        }
+        cout << cnt << endl;
+    }
+    return 0;
+}
