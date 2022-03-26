e3466659502a19098c24d8b6dcb7c061e5a9df48
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-03-26 23:55:08 +0900

    A

---
+++ exercises/atcoder/abc/245/a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+#define rep(i,n) for(int i = 0; i < (n); i++)
+#define IntToStr(n) to_string(n) //整数を文字列に変換
+#define StrToInt(s) stoi(s) //文字列を整数に変換 
+#define rev(s) reverse(s.begin(), s.end()) //文字列反転
+#define out(n) cout << n << endl
+
+/*
+queue: https://cpprefjp.github.io/reference/queue/queue.html
+stack: https://cpprefjp.github.io/reference/stack.html
+permutation: https://cpprefjp.github.io/reference/algorithm/next_permutation.html
+*/
+
+const ll mod = 1e9 + 7;
+const int INF = 1000000001;
+
+void solve(){
+
+}
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int T = 3600*a + 60*b;
+    int A = 3600*c + 60*d + 1;
+    if(T < A){
+        cout << "Takahashi" << endl;
+    }else{
+        cout << "Aoki" << endl;
+    }
+    return 0;
+}
