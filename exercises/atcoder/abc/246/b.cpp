9f32f0a3470e4a56f557274add6d0214c65e1706
KeiTaylor0606 <mvemtae5757@outlook.jp>
2022-04-03 02:17:40 +0900

    B

---
+++ exercises/atcoder/abc/246/b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+using P = pair<int, int>;
+using Graph = vector<vector<int>>;
+
+#define rep(i,n) for(int i = 0; i < (n); i++)
+#define IntToStr(n) to_string(n) //整数を文字列に変換
+#define StrToInt(s) stoi(s) //文字列を整数に変換 
+#define rev(s) reverse(s.begin(), s.end()) //文字列反転
+#define out(n) cout << n << endl
+
+/*
+queue: https://cpprefjp.github.io/reference/queue/queue.html
+stack: https://cpprefjp.github.io/reference/stack.html
+permutation: https://cpprefjp.github.io/reference/algorithm/next_permutation.html
+*/
+
+const ll mod = 1e9 + 7;
+const int INF = 1000000001;
+
+void solve(){
+
+}
+
+int main(){
+    int A, B;
+    cin >> A >> B;
+    double dist = sqrt(A*A + B*B);
+    cout << fixed << setprecision(12) << (double)(A/dist) << " " << (double)(B/dist) << endl;
+    return 0;
+}
